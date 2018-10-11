9dcbf6261338af9fbe14207eeebab3b0c77147eb
KavyaSirivolu <kavyasirivolu@gmail.com>
2018-10-11 13:07:48 -0500

    Assingment Q2

---
+++ Q2rawinput.py
@@ -0,0 +1,11 @@
+x= int(raw_input("enter 1st number"))
+print type(x)
+
+y=float(raw_input("enter 2nd number"))
+print type(y)
+
+z=raw_input("enter 3rd value")
+print type(z)
+
+k= complex(raw_input("enter 4th number"))
+print type(k)
