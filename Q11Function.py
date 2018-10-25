fed13c747d4803e8d29e851d43ab1437edba648a
KavyaSirivolu <kavyasirivolu@gmail.com>
2018-10-25 12:56:47 -0500

    Q11 -Function

---
+++ Q11Function.py
@@ -0,0 +1,21 @@
+def add(a,b):
+	return a+b
+
+def sub(c,d):
+	return c-d
+e=sub(30,20)
+
+def mul(a,b,c):
+	return(a*b*c)
+c=mul(20,5,4 )
+
+
+def exp(c,d):
+	return c**d
+c=exp(2,5)
+
+
+def div(a,b):
+	return (a/b)
+c=div(20,5)
+
